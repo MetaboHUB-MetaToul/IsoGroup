0df76ea1eafaa5ea40897edbff0428b6982478bd
legregam <legregam@insa-toulouse.fr>
2024-12-03 11:50:05 +0100

    Added init from file

--- isogroup/sample.py
+++ isogroup/sample.py
@@ -2,11 +2,16 @@ import pandas as pd
 
 
 class Sample:
-    def __init__(self, dataset: pd.DataFrame, type: str):
+    def __init__(self, dataset: pd.DataFrame, sample_type: str):
         self.dataset = dataset
-        self.type = type
+        self.sample_type = sample_type
         self.clusters = []
         self.nb_clusters = len(self.clusters)
+
+    @classmethod
+    def from_file(cls, path: str, sample_type: str):
+        dataset = pd.read_csv(path)
+        return cls(dataset, sample_type)
 
     def initialize_clusters(self):
         pass
