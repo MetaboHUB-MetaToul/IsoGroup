bdb9c6a878a18f8d20137f1685855adc2405d9aa
legregam <legregam@insa-toulouse.fr>
2025-02-03 16:39:35 +0100

    Added some type annotations

--- isogroup/base/experiment.py
+++ isogroup/base/experiment.py
@@ -2,16 +2,18 @@ import pandas as pd
 from isogroup.base.database import Database
 from isogroup.base.sample import Sample
 from isogroup.base.feature import Feature
+
+
 class Experiment:
 
-    def __init__(self, dataset: pd.DataFrame, database: Database=None):
+    def __init__(self, dataset: pd.DataFrame, database: Database = None):
         self.experiment = dataset
         self.database = database
-        self.samples = {}
-        self.annotated_experiment = None
+        self.samples: dict = {}
+        self.annotated_experiment: None | pd.DataFrame = None
         self.initialize_samples()
-        self.mz_tol = None
-        self.rt_tol = None
+        self.mz_tol: None | float = None
+        self.rt_tol: None | float = None
 
     def annotate_experiment(self, mz_tol, rt_tol):
 
@@ -19,14 +21,14 @@ class Experiment:
         for idx, _ in self.experiment.iterrows():
             mz = idx[0]
             rt = idx[1]
-            id = idx[2]
+            identity = idx[2]
             dummy_feature = Feature(rt=rt, mz=mz, intensity=None)
-            annotated_row = [mz, rt, id, [], []]
+            annotated_row = [mz, rt, identity, [], []]
             for feature in self.database.features:
                 if ((abs(
                         float(feature.mz) - dummy_feature.mz) <=
-                    mz_tol and abs(float(feature.rt) - dummy_feature.rt) <=
-                        rt_tol)):
+                     mz_tol and abs(float(feature.rt) - dummy_feature.rt) <=
+                     rt_tol)):
                     annotated_row[3].append(feature.metabolite)
                     annotated_row[4].append(feature.isotopologue)
             annotated_row[3] = str(annotated_row[3])
@@ -50,18 +52,16 @@ class Experiment:
                                           sample_type="test")
         print(self.samples)
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     data = pd.read_csv(
         r"C:\Users\legregam\PycharmProjects\IsoGroup\data\dataset_test.txt",
         sep="\t"
     )
     data = data.set_index(["mz", "rt", "id"])
     db_data = pd.read_csv(r"C:\Users\legregam\PycharmProjects\IsoGroup\data"
-                       r"\database_test.csv", sep=";")
+                          r"\database_test.csv", sep=";")
     database = Database(dataset=db_data)
     experiment = Experiment(dataset=data, database=database)
     experiment.annotate_experiment(mz_tol=0.01, rt_tol=10)
     print(experiment.annotated_experiment)
-
-
