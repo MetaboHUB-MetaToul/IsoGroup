9e5eae7be8e21e96041cc5e2e679c59b5f472759
n-butin <butin@insa-toulouse.fr>
2025-02-19 07:59:40 +0100

    Store experimental features and annotations

--- isogroup/base/experiment.py
+++ isogroup/base/experiment.py
@@ -1,26 +1,25 @@
-
-"""
-New version test
-"""
-
 import pandas as pd
 from isogroup.base.database import Database
 from isogroup.base.sample import Sample
 from isogroup.base.feature import Feature
+from isogroup.base.cluster import Cluster
 
 class Experiment:
 
     def __init__(self, dataset: pd.DataFrame, database: 'Database' = None):
         self.experiment = dataset
         self.database = database
-        self.samples: dict = {}
+        self.samples: dict = {} # Dictionary to store the samples
         self.annotated_experiment: None | pd.DataFrame = None
+        self.clusters_summary: None | pd.DataFrame = None # Summary of the clusters of annotated features in the experiment
         self.mz_tol: None | float = None
         self.rt_tol: None | float = None
-        self.metabolite_clusters: None | pd.DataFrame = None
+
+        self.feature_exp: list = [] # List of experiment features
+        self.annotated_clusters: list = []   # List of annotated clusters
+
 
     def annotate_experiment(self, mz_tol, rt_tol):
-
         """
         Annotate the experiment features with the database within a given tolerance
         And calculate the mz error and the rt error.
@@ -33,9 +32,10 @@ class Experiment:
             rt = idx[1]
             identity = idx[2]
 
-            # Initialize experiment features
-            exp_feature = Feature(rt=rt, mz=mz, intensity=None)
-            
+            # Initialize the experiment features from the dataset
+            feature_data = Feature(rt=rt, mz=mz, intensity=None, metabolite=None, isotopologue=None)
+
+            # Initialize lists to store the metabolites, isotopologues, mz errors and rt errors
             metabolites, isotopologues, mz_errors, rt_errors = [], [], [], []
             
             for feature_db in self.database.features:
@@ -43,8 +43,8 @@ class Experiment:
                 rt_db = float(feature_db.rt)
 
                 # Calculate the exact mz and rt errors
-                mz_error = (mz_db - exp_feature.mz)
-                rt_error = (rt_db - exp_feature.rt)
+                mz_error = (mz_db - feature_data.mz)
+                rt_error = (rt_db - feature_data.rt)
 
                 # Check if the feature is within tolerance
                 if abs(mz_error) <= mz_tol and abs(rt_error) <= rt_tol:
@@ -53,9 +53,14 @@ class Experiment:
                     isotopologues.append(feature_db.isotopologue)
                     mz_errors.append(mz_error)
                     rt_errors.append(rt_error)
-                
+ 
             annotated_data.append([mz, rt, identity, metabolites, isotopologues, mz_errors, rt_errors])
             
+            # Store all annotations in feature_data
+            feature_data.metabolite = metabolites
+            feature_data.isotopologue = isotopologues
+            self.feature_exp.append(feature_data)
+                
         self.mz_tol = mz_tol
         self.rt_tol = rt_tol
         self.annotated_experiment = pd.DataFrame(
@@ -63,8 +68,9 @@ class Experiment:
             columns=["mz", "rt", "id", "metabolite", "isotopologues", "mz_error", "rt_error"]
         ).set_index(["mz", "rt", "id"])
 
-        # Export the annotated experiment to an Excel file
-        self.annotated_experiment.to_excel("annotated_experiment.xlsx", index=True)  
+        # Export the annotated experiment to a tsv file
+        self.annotated_experiment.to_csv("annotated_experiment.tsv", sep="\t")
+
 
     def initialize_samples(self):
         """
@@ -74,11 +80,12 @@ class Experiment:
         data = self.annotated_experiment if (self.annotated_experiment is not None) else self.experiment
         for sample in data.columns:
             self.samples[sample] = Sample(dataset=data[[sample]], sample_type="test")
-        
+ 
 
-    def export_to_excel(self, filename="annotated_experiment.xlsx"):
-        """ Export the annotated experiment to an Excel file """
-        if self.annotated_experiment is not None:
-            df_export = self.annotated_experiment.copy()
-            df_export.to_excel(filename, index=True)
+
+    def get_metabolite_clusters(self):
+        """
+        Create metabolite clusters from the feature_exp in the annotated experiment (if unique annotation)
+        """
+
 
