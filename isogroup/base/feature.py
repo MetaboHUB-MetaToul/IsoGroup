5da8e1881a36b51b5585348bfd93e6efd9947816
n-butin <butin@insa-toulouse.fr>
2025-02-19 10:57:11 +0100

    Added attributes (mz_error, rt_error)

--- isogroup/base/feature.py
+++ isogroup/base/feature.py
@@ -1,11 +1,15 @@
 class Feature:
 
-    def __init__(self, rt: float, mz: float, intensity: float|None,
+    def __init__(self, rt: float, mz: float, intensity: float|None, Fid = None,
+                 mz_error=None, rt_error=None,
                  metabolite=None, isotopologue=None, **extra_dims):
 
         self.rt = rt
         self.mz = mz
         self.intensity = intensity
+        self.Fid = Fid
+        self.mz_error = mz_error
+        self.rt_error = rt_error
         self.metabolite = metabolite
         self.isotopologue = isotopologue
         self.__dict__.update(extra_dims)
@@ -17,7 +21,7 @@ class Feature:
         Return a string representation of the feature.
         :return: str
         """
-        return (f", Feature(RT={self.rt}, Metabolite={self.metabolite} "
+        return (f", Feature(Fid = {self.Fid}, RT={self.rt}, Metabolite={self.metabolite}, Isotopologue={self.isotopologue}, "
                 f"mz={self.mz}, "
                 f"intensity={self.intensity})")
 
