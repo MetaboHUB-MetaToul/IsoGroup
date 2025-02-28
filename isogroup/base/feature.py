0ab286776f035115d366c2c52f79b62eedbd35cb
n-butin <butin@insa-toulouse.fr>
2025-02-28 13:45:36 +0100

    Added intensities as a dictionnary

--- isogroup/base/feature.py
+++ isogroup/base/feature.py
@@ -1,13 +1,12 @@
 class Feature:
 
-    def __init__(self, rt: float, mz: float, intensity: float|None, Fid = None,
-                 mz_error=None, rt_error=None,
-                 metabolite=None, isotopologue=None, **extra_dims):
-
+    def __init__(self, rt: float, mz: float, intensity: dict[float]|None, feature_id = None, formula: list|None=None,
+                 metabolite: list|None=None, isotopologue: list|None=None, mz_error: list|None=None, rt_error: list|None=None, **extra_dims):
         self.rt = rt
         self.mz = mz
         self.intensity = intensity
-        self.Fid = Fid
+        self.feature_id = feature_id
+        self.formula = formula
         self.mz_error = mz_error
         self.rt_error = rt_error
         self.metabolite = metabolite
@@ -21,9 +20,16 @@ class Feature:
         Return a string representation of the feature.
         :return: str
         """
-        return (f", Feature(Fid = {self.Fid}, RT={self.rt}, Metabolite={self.metabolite}, Isotopologue={self.isotopologue}, "
+        return (f", Feature(Identity = {self.feature_id}, RT={self.rt}, Metabolite={self.metabolite}, Isotopologue={self.isotopologue}, Formula={self.formula}, "
                 f"mz={self.mz}, "
                 f"intensity={self.intensity})")
+    
 
 
+# class AnnotatedFeature(Feature):
+
+#     def __init__(self, mz_error: float, rt_error: float, **kwargs):
+#         super().__init__(**kwargs)
+#         self.mz_error = mz_error
+#         self.rt_error = rt_error
 
