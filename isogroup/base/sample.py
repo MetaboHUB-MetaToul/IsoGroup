48ee99e94834cae200227db82e5c4ec09484ca27
butin <butin@insa-toulouse.fr>
2025-03-09 16:19:39 +0100

    Updated Sample class

--- isogroup/base/sample.py
+++ isogroup/base/sample.py
@@ -14,25 +14,7 @@ class Sample:
         Initialize the features for this sample from the annotated data.
         For each feature, create a new instance with the intensity of the sample.
         """
-        for feature in annotated_data:
-            # Extract the intensity of the sample
-            feature_intensity = {self.sample_name: feature.intensity[self.sample_name]}
-
-            # Create a new feature for this sample (copy the feature and update the intensity)
-            new_feature = Feature(
-                rt=feature.rt,
-                mz=feature.mz,
-                feature_id=feature.feature_id,
-                intensity=feature_intensity,
-                formula=feature.formula,
-                metabolite=feature.metabolite,
-                isotopologue=feature.isotopologue,
-                mz_error=feature.mz_error,
-                rt_error=feature.rt_error
-            )
-
-            # Add the new feature to the sample
-            self.features.append(new_feature)
+        pass
 
     def __repr__(self):
         """
@@ -40,5 +22,4 @@ class Sample:
         :return: str
         """
         return f"Sample({self.sample_name}, {self.sample_type}, {self.features})"
-
-
+    
