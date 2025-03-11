1100107697dd4b0ea321b24afc96cc50814c9774
n-butin <butin@insa-toulouse.fr>
2025-03-11 10:16:55 +0100

    Updated export methods

--- isogroup/base/experiment.py
+++ isogroup/base/experiment.py
@@ -16,7 +16,8 @@ class Experiment:
         self.experimental_features : list = [] # List of experimental features
         self.annotated_features: list = [] # List of experimental features after annotation # Inutile ? Modification de l'objet feature directement dans la liste experimental_features
         self.annotated_clusters: list = []   # List of annotated clusters 
-
+        self. annotated_sample_clusters: list = [] # List of annotated custers for each sample
+        
         self.annotated_experiment: None | pd.DataFrame = None # Rename
 
 
@@ -103,32 +104,37 @@ class Experiment:
         self.tracer_element = tracer_element
 
 
-    def features_summary(self, filename = None, sample_name = None):
+    def features_export(self, filename = None, sample_name = None):
         """
         Create a DataFrame to summarize the annotated data
-        Optionnal: Export the DataFrame to a tsv file if a filename is provided
+        Optionnal: Export the DataFrame to a tsv file if a filename is provided with samples in column
         Optionnal: Export the Dataframe of only one sample if a sample name is provided
         """
 
         # Create a DataFrame to summarize the experimental features
         df = pd.DataFrame([vars(f) for f in self.experimental_features])
-        df = df.drop(columns=["is_adduct", "in_cluster"], errors="ignore")
+        df = df.drop(columns=["is_adduct", "in_cluster", "metabolite", "formula"], errors="ignore")
 
+        columns_to_fix = ["annotation", "mz_error", "rt_error", "isotopologue"]
+        for col in columns_to_fix:
+            df[col] = df[col].apply(lambda x: ", ".join(map(str, x)) if isinstance(x, list) else str(x))
+        df = df.pivot_table(index=["mz", "rt", "feature_id", "annotation", "mz_error", "rt_error", "isotopologue"],
+                                  columns="sample", values="intensity",
+                                  aggfunc="first").reset_index()
+        
         self.annotated_experiment = df
 
         # Export the DataFrame to a csv file if a filename is provided
         if filename:
-            df = df.drop(columns=["metabolite"], errors="ignore")
             df.to_csv(filename, sep="\t", index=False)
 
         # Export the Dataframe of only one sample if a sample name is provided
         if filename and sample_name:
             # Check if the sample name is in the DataFrame
-            if sample_name not in df["sample"].unique():
+            if sample_name not in df.columns:
                 raise ValueError(f"The sample {sample_name} is not in the DataFrame")
             
-            df = df[df["sample"] == sample_name]
-            df = df.drop(columns=["metabolite"], errors="ignore")
+            df = df[["mz", "rt", "feature_id", "annotation", "mz_error", "rt_error", "isotopologue", sample_name]]
             df.to_csv(filename, sep="\t", index=False)
 
         return df
@@ -157,13 +163,8 @@ class Experiment:
             for i, (metabolite_name, features) in enumerate(clusters.items(), start=0)
             ]
 
-        # self.annotated_clusters = [
-        #     Cluster(features, cluster_id=i)
-        #     for i, (metabolite_name, features) in enumerate(clusters.items(), start=0)
-        #     ]
 
-
-    def cluster_summary(self, filename = None, sample_name = None):
+    def cluster_export(self, filename = None, sample_name = None):
         """
         Create a DataFrame to summarize the annotated clusters
         Optionnal: Export the DataFrame to a tsv file if a filename is provided
@@ -193,39 +194,54 @@ class Experiment:
                     "feature_id": feature.feature_id,
                     "mz": feature.mz,
                     "rt": feature.rt,
-                    "feature_possible_annotation": feature.annotation,
-                    "isolopologue": feature.isotopologue,
-                    "mz_error": feature.mz_error,
-                    "rt_error": feature.rt_error,
+                    "feature_possible_annotation": feature.annotation if isinstance(feature.annotation, str) else str(feature.annotation),
+                    "isotopologue": feature.isotopologue if isinstance(feature.isotopologue, str) else str(feature.isotopologue),
+                    "mz_error": feature.mz_error if isinstance(feature.mz_error, str) else str(feature.mz_error),
+                    "rt_error": feature.rt_error if isinstance(feature.rt_error, str) else str(feature.rt_error),
                     "sample": feature.sample,
                     "intensity": feature.intensity,
                     "is_complete": cluster.is_complete,
-                    "missing_isotopologue": cluster.missing_isotopologue
+                    "missing_isotopologue": cluster.missing_isotopologue if isinstance(cluster.missing_isotopologue, str) else str(cluster.missing_isotopologue)
                 })
 
         # Create a DataFrame to summarize the annotated clusters
         df = pd.DataFrame(cluster_data)
 
-        # Check if the DataFrame is empty
-        # if df.empty:
-        #     raise ValueError("No data found for the provided sample") 
+        df_pivot = df.pivot_table(index=["cluster_id", "annotation", "feature_id", "mz", "rt", "feature_possible_annotation", "isotopologue", "mz_error", "rt_error", "is_complete", "missing_isotopologue"],
+                                  columns="sample", values="intensity",
+                                  aggfunc="first").reset_index()
 
         # Export the DataFrame to a tsv file if a filename is provided
         if filename:
-            df.to_csv(filename, sep="\t", index=False)
+            df_pivot.to_csv(filename, sep="\t", index=False)
 
-        return df
+        return df_pivot
 
 
 
-    def feature_by_id(self):
+# Methods modified to create individual clusters objects by sample
+
+    def get_annotated_clusters_by_sample(self):
         """
-        Return all the features with the same feature_id
+        Create clusters by metabolite from the annotated features
+        Each cluster groups features with the same annotation for a given sample
         """
-        pass
+        # Check if the experiment has been annotate
+        if not self.annotated_features:
+            raise ValueError("The experiment has not been annotated yet")
+        
+        # Create a dictionary to store the clusters
+        clusters = {}
+        for feature in self.annotated_features:
+            for metabolite_name in feature.annotation:
+                sample = feature.sample
+                cluster_key = (metabolite_name, sample)
+                if cluster_key not in clusters:
+                    clusters[cluster_key] = []
+                clusters[cluster_key].append(feature)
 
-    def feature_by_sample(self):
-        """
-        Return all the features for a specific sample
-        """
-        pass
+        # Create a Cluster object for each cluster and add a cluster id
+        self.annotated_sample_clusters = [
+            Cluster(features, cluster_id=i, cluster_annotation=metabolite_name, tracer=self.tracer, tracer_element=self.tracer_element)
+            for i, ((metabolite_name, sample), features) in enumerate(clusters.items(), start=0)
+            ]
