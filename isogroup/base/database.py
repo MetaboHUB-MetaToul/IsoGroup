6e3369ebb5c231828feb533733ef740bb30cb98e
n-butin <butin@insa-toulouse.fr>
2025-03-12 17:14:01 +0100

    revised nomenclature

--- isogroup/base/database.py
+++ isogroup/base/database.py
@@ -1,25 +1,24 @@
 from isogroup.base.feature import Feature
 from isocor.base import LabelledChemical
-from isogroup.base.cluster import Cluster
+from isogroup.base.misc import Misc
 import pandas as pd
 
 
 class Database:
 
-    def __init__(self, dataset: pd.DataFrame, tracer="13C", tracer_element="C"):
+    def __init__(self, dataset: pd.DataFrame, tracer="13C"):
         self.dataset = dataset
         self.features: list = []
         self.tracer: str = tracer
-        self.tracer_element: str = tracer_element
+        self._tracer_element, self._tracer_idx = Misc._parse_strtracer(tracer)
         self.clusters: list = []
 
         _isodata: dict = LabelledChemical.DEFAULT_ISODATA
-        self._delta_mz_tracer: float = _isodata[tracer_element]["mass"][1] - _isodata[
-            tracer_element]["mass"][0]
+        self._delta_mz_tracer: float = _isodata[self._tracer_element]["mass"][1] - _isodata[
+            self._tracer_element]["mass"][0]
         self._delta_mz_hydrogen: float = _isodata["H"]["mass"][0]
 
         self.initialize_theoretical_features()
-        #self.initialize_theoretical_clusters()
 
     def __len__(self) -> int:
         return len(self.dataset)
@@ -41,7 +40,7 @@ class Database:
                 charge=line["charge"],
                 label=line["metabolite"],
             )
-            for i in range(chemical.formula[self.tracer_element] + 1):
+            for i in range(chemical.formula[self._tracer_element] + 1):
                 mz = (chemical.molecular_weight + i * self._delta_mz_tracer
                       + line["charge"] * self._delta_mz_hydrogen)
                 feature = Feature(
@@ -49,23 +48,8 @@ class Database:
                     mz=mz,
                     tracer=self.tracer,
                     intensity=None,
-                    metabolite=[chemical],
-                    isotopologue=[i]
+                    chemical=[chemical],
+                    isotopologue=[i],
+                    metabolite=[chemical.label]
                 )
                 self.features.append(feature)
-
-
-    # def initialize_theoretical_clusters(self):
-    #     """
-    #     Creates theoretical clusters from the theoretical features
-    #     Clusters are created by grouping features with the same metabolite
-    #     """
-    #     metabolite_dict = {}
-    #     for feature in self.features:
-    #         if feature.metabolite not in metabolite_dict:
-    #             metabolite_dict[feature.metabolite] = []
-    #         metabolite_dict[feature.metabolite].append(feature)
-
-    #     for metabolite, features in metabolite_dict.items():
-    #         cluster = Cluster(features, tracer_element=self.tracer_element)
-    #         self.clusters.append(cluster)
