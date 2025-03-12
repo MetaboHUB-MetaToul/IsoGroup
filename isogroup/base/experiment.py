500d3d892d5117a74c83a011ad2bff4985ea268b
n-butin <butin@insa-toulouse.fr>
2025-03-12 11:45:29 +0100

    Updated Experiment class

--- isogroup/base/experiment.py
+++ isogroup/base/experiment.py
@@ -2,24 +2,75 @@ import pandas as pd
 from isogroup.base.database import Database
 from isogroup.base.feature import Feature
 from isogroup.base.cluster import Cluster
+from isocor.base import LabelledChemical
+import re
 
 class Experiment:
 
-    def __init__(self, dataset: pd.DataFrame, database: 'Database' = None):
+    def __init__(self, dataset: pd.DataFrame, database: 'Database' = None, tracer=None):
         self.dataset = dataset
         self.database = database
         self.samples: dict = {} # Dictionary to store the samples
-        self.mz_tol: None | float = None
-        self.rt_tol: None | float = None
-        self.tracer: None | str = None
-        self.tracer_element: None | str = None
-        self.experimental_features : list = [] # List of experimental features
-        self.annotated_features: list = [] # List of experimental features after annotation # Inutile ? Modification de l'objet feature directement dans la liste experimental_features
-        self.annotated_clusters: list = []   # List of annotated clusters 
-        self. annotated_sample_clusters: list = [] # List of annotated custers for each sample
-        
-        self.annotated_experiment: None | pd.DataFrame = None # Rename
+        self._mz_tol: None | float = None
+        self._rt_tol: None | float = None
 
+        self._tracer: None | str = tracer
+        self._tracer_element, self._tracer_idx = self._parse_strtracer(tracer)
+        #self.experimental_features : list = [] # List of experimental features
+        #self.annotated_features: list = [] # List of experimental features after annotation # Inutile ? Modification de l'objet feature directement dans la liste experimental_features
+        #self.annotated_clusters: list = []   # List of annotated clusters 
+        #self.annotated_sample_clusters: list = [] # List of annotated custers for each sample
+        self.clusters = None
+        #self.annotated_experiment: None | pd.DataFrame = None # Rename
+        
+
+    @property
+    def rt_tol(self):
+        return self._rt_tol
+
+    @property
+    def tracer(self):
+        return self._tracer
+
+    @property
+    def tracer_element(self):
+        return self._tracer_element
+
+    @property
+    def mz_tol(self):
+        return self._mz_tol
+    
+    @staticmethod
+    def _parse_strtracer(str_tracer):
+        """Parse the tracer code.
+
+        Args:
+            str_tracer (str): tracer code (e.g. "13C")
+
+        Returns:
+            tuple
+                - (str) tracer element (e.g. "C")
+                - (int) tracer index in :py:attr:`~data_isotopes`
+        """
+        try:
+            tracer = re.search(r'(\d*)([A-Z][a-z]*)', str_tracer)
+            count = int(tracer.group(1))
+            tracer_el = tracer.group(2)
+        except (ValueError, AttributeError):
+            raise ValueError("Invalid tracer code: '{}'."
+                             " Please check your inputs.".format(str_tracer))
+        best_diff = float("inf")
+        idx_tracer = None
+        unexpected_msg = "Unexpected tracer code. Are you sure this isotope is "\
+                         "in data_isotopes? '{}'".format(str_tracer)
+        assert tracer_el in LabelledChemical.DEFAULT_ISODATA, unexpected_msg
+        for i, mass in enumerate(LabelledChemical.DEFAULT_ISODATA[tracer_el]["mass"]):
+            test_diff = abs(mass - count)
+            if test_diff < best_diff:
+                best_diff = test_diff
+                idx_tracer = i
+        assert best_diff < 0.5, unexpected_msg
+        return (tracer_el, idx_tracer)
 
     def initialize_experimental_features(self):
         """
@@ -28,33 +79,34 @@ class Experiment:
         for idx, _ in self.dataset.iterrows():
             mz = idx[0]
             rt = idx[1]
-            identity = idx[2]
+            id = idx[2]
 
             # Extract the intensity for each sample in the dataset
             for sample in self.dataset.columns:
-                if sample not in ["mz", "rt", "identity"]:
+                if sample not in ["mz", "rt", "id"]:
                     intensity = self.dataset.loc[idx, sample]
 
                     # Initialize the experimental features for each sample
                     feature = Feature(
-                        rt=rt, mz=mz, 
-                        feature_id=identity, 
+                        rt=rt, mz=mz, tracer=self.tracer,
+                        feature_id=id, 
                         intensity=intensity,
                         metabolite=[],
                         annotation=[],
                         isotopologue=[],
                         mz_error=[],
                         rt_error=[],
+                        formula=[],
                         sample=sample
                         ) 
                     
                     # Add the feature in the list corresponding to the sample
                     if sample not in self.samples:
-                        self.samples[sample] = []
-                    self.samples[sample].append(feature)
+                        self.samples[sample] = {}
+                    self.samples[sample][id] = feature
                     
                     # Store all experimental features
-                    self.experimental_features.append(feature)
+                    #self.experimental_features.append(feature)
 
 
     def annotate_features(self, mz_tol, rt_tol):
@@ -62,34 +114,30 @@ class Experiment:
         Annotate the experiment features with the database within a given tolerance
         Calculate the mz error and the rt error
         """
-        for feature in self.experimental_features:
-            mz = feature.mz
-            rt = feature.rt
 
-            for th_feature in self.database.features:
-                mz_db = float(th_feature.mz)
-                rt_db = float(th_feature.rt)
+        for sample in self.samples.values():
+            for feature in sample.values():
+                
+                for db_feature in self.database.features:
 
-                # Calculate the exact mz and rt errors
-                mz_error = (mz_db - mz)
-                rt_error = (rt_db - rt)
+                    # Calculate the exact mz and rt errors
+                    mz_error = (db_feature.mz - feature.mz)
+                    rt_error = (db_feature.rt - feature.rt)
 
-                # Check if the experimental feature is within tolerance
-                if abs(mz_error) <= mz_tol and abs(rt_error) <= rt_tol:
-                    feature.metabolite.append(th_feature.metabolite)
-                    feature.isotopologue.append(th_feature.isotopologue)
-                    feature.annotation.append(th_feature.metabolite.label)
-                    feature.mz_error.append(mz_error)
-                    feature.rt_error.append(rt_error)
+                    # Check if the experimental feature is within tolerance
+                    if abs(mz_error) <= mz_tol and abs(rt_error) <= rt_tol:
+                        feature.metabolite.append(db_feature.metabolite[0])
+                        feature.isotopologue.append(db_feature.isotopologue[0])
+                        feature.annotation.append(db_feature.metabolite[0].label)
+                        feature.formula.append(db_feature.metabolite[0].formula)
+                        feature.mz_error.append(mz_error)
+                        feature.rt_error.append(rt_error)
 
-            # Store all experimental features after annotation
-            self.annotated_features.append(feature)  # Inutile ? Modification de l'objet feature directement dans la liste experimental_features, sample mis à jour
-
-        self.mz_tol = mz_tol
-        self.rt_tol = rt_tol
+        self._mz_tol = mz_tol
+        self._rt_tol = rt_tol
 
 
-    def annotate_experiment(self, mz_tol, rt_tol, tracer, tracer_element):
+    def annotate_experiment(self, mz_tol, rt_tol):
         """
         Annotate the experiment features with the database within a given tolerance
         MultiIndex DataFrame
@@ -100,8 +148,6 @@ class Experiment:
         # Annotate the experimental features
         self.annotate_features(mz_tol, rt_tol)
 
-        self.tracer = tracer
-        self.tracer_element = tracer_element
 
 
     def features_export(self, filename = None, sample_name = None):
@@ -138,110 +184,81 @@ class Experiment:
             df.to_csv(filename, sep="\t", index=False)
 
         return df
+    
+    def clusterize(self):
+        """
+        Create unique clusters from annotated features based on their names.
+        """
+        cluster_names = []
+        
+        # Group features by metabolite
+        for sample in self.samples.values():
+            for feature in sample.values():
+                cluster_names += feature.annotation
+
+        cluster_names = set(cluster_names)
+        
+        # Create unique clusters
+        self.clusters = {}
+        for sample in self.samples.keys():
+            self.clusters[sample] = {}
+            for i,c in enumerate(cluster_names):
+                features = self.get_features_from_name(c, sample)
+                self.clusters[sample][c] = Cluster(features=features, cluster_id=f"C{i}", name=c)
+
+
+    def get_features_from_name(self, name, sample_name:str):
+        """
+        Get a feature from the experiment by its name, in a given sample if provided
+        """
+        features = []
+        for feature in self.samples[sample_name].values():
+            if name in feature.annotation:
+                features.append(feature)
+        return features
 
     
-    def get_annotated_clusters(self):
-        """
-        Create clusters by metabolite from the annotated features
-        Duplicate the features if they have multiple annotations to create a cluster for each annotation
-        """
-        # Check if the experiment has been annotated
-        if not self.annotated_features:
-            raise ValueError("The experiment has not been annotated yet")
-        
-        # Create a dictionary to store the clusters
-        clusters = {}
-        for feature in self.experimental_features:
-            for metabolite_name in feature.annotation: # A feature can have multiple annotations
-                if metabolite_name not in clusters:
-                    clusters[metabolite_name] = []
-                clusters[metabolite_name].append(feature)
-                
-        # Create a Cluster object for each cluster and add a cluster id
-        self.annotated_clusters = [
-            Cluster(features, cluster_id=i, cluster_annotation=metabolite_name, tracer=self.tracer, tracer_element=self.tracer_element)
-            for i, (metabolite_name, features) in enumerate(clusters.items(), start=0)
-            ]
-
-
-    def cluster_export(self, filename = None, sample_name = None):
+    def export_clusters(self, filename = None, sample_name = None):
         """
         Create a DataFrame to summarize the annotated clusters
         Optionnal: Export the DataFrame to a tsv file if a filename is provided
         Optionnal: Export the Dataframe of only one sample if a sample name is provided
         """
-        # Check if the experiment has been annotated
-        if not self.annotated_clusters:
-            raise ValueError("No annotated clusters found. Run get_annotated_clusters() first")
         
         # Check if the sample name is in the DataFrame
-        all_samples = {f.sample for c in self.annotated_clusters for f in c.features}
-        if sample_name and sample_name not in all_samples:
-            raise ValueError(f"Sample {sample_name} not found in annotated clusters. Available samples: {', '.join(all_samples)}")
+        all_samples = list(self.samples.keys())
+        if sample_name is not None:
+            if sample_name not in all_samples:
+                raise ValueError(f"Sample {sample_name} not found in annotated clusters. Available samples: {', '.join(all_samples)}")
         
-        cluster_data = [] # List to store the cluster data
-
-        for cluster in self.annotated_clusters:
-            for feature in cluster.features:
-            
-                # Apply filter on the sample name if provided
-                if sample_name and feature.sample != sample_name:
-                    continue
-
-                cluster_data.append({
-                    "cluster_id": cluster.cluster_id,
-                    "annotation": cluster.cluster_annotation,
-                    "feature_id": feature.feature_id,
-                    "mz": feature.mz,
-                    "rt": feature.rt,
-                    "feature_possible_annotation": feature.annotation if isinstance(feature.annotation, str) else str(feature.annotation),
-                    "isotopologue": feature.isotopologue if isinstance(feature.isotopologue, str) else str(feature.isotopologue),
-                    "mz_error": feature.mz_error if isinstance(feature.mz_error, str) else str(feature.mz_error),
-                    "rt_error": feature.rt_error if isinstance(feature.rt_error, str) else str(feature.rt_error),
-                    "sample": feature.sample,
-                    "intensity": feature.intensity,
-                    "is_complete": cluster.is_complete,
-                    "missing_isotopologue": cluster.missing_isotopologue if isinstance(cluster.missing_isotopologue, str) else str(cluster.missing_isotopologue)
-                })
+        cluster_data = []
+        for sample, clusters in self.clusters.items():
+            for cname, cluster in clusters.items():
+                for feature in cluster.features:
+                    idx = [i for i,j in enumerate(feature.annotation) if j == cname][0]
+                    cluster_data.append({
+                        "cluster_id": cluster.cluster_id,
+                        "annotation": cluster.name,
+                        "feature_id": feature.feature_id,
+                        "mz": feature.mz,
+                        "rt": feature.rt,
+                        "feature_possible_annotation": feature.annotation,
+                        "isotopologue": feature.isotopologue[idx],
+                        "mz_error": feature.mz_error[idx],
+                        "rt_error": feature.rt_error[idx],
+                        "sample": feature.sample,
+                        "intensity": feature.intensity,
+                        "is_complete": "TODO",#cluster.is_complete,
+                        "missing_isotopologue": "TODO"# cluster.missing_isotopologue
+                    })
 
         # Create a DataFrame to summarize the annotated clusters
         df = pd.DataFrame(cluster_data)
 
-        df_pivot = df.pivot_table(index=["cluster_id", "annotation", "feature_id", "mz", "rt", "feature_possible_annotation", "isotopologue", "mz_error", "rt_error", "is_complete", "missing_isotopologue"],
-                                  columns="sample", values="intensity",
-                                  aggfunc="first").reset_index()
-
         # Export the DataFrame to a tsv file if a filename is provided
         if filename:
-            df_pivot.to_csv(filename, sep="\t", index=False)
+            df.to_csv(filename, sep="\t", index=False)
 
-        return df_pivot
+        return df
 
 
-
-# Methods modified to create individual clusters objects by sample
-
-    def get_annotated_clusters_by_sample(self):
-        """
-        Create clusters by metabolite from the annotated features
-        Each cluster groups features with the same annotation for a given sample
-        """
-        # Check if the experiment has been annotate
-        if not self.annotated_features:
-            raise ValueError("The experiment has not been annotated yet")
-        
-        # Create a dictionary to store the clusters
-        clusters = {}
-        for feature in self.annotated_features:
-            for metabolite_name in feature.annotation:
-                sample = feature.sample
-                cluster_key = (metabolite_name, sample)
-                if cluster_key not in clusters:
-                    clusters[cluster_key] = []
-                clusters[cluster_key].append(feature)
-
-        # Create a Cluster object for each cluster and add a cluster id
-        self.annotated_sample_clusters = [
-            Cluster(features, cluster_id=i, cluster_annotation=metabolite_name, tracer=self.tracer, tracer_element=self.tracer_element)
-            for i, ((metabolite_name, sample), features) in enumerate(clusters.items(), start=0)
-            ]
