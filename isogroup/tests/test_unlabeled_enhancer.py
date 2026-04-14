a1fe384d5e2aac9f3bd7cb887af210745aced82d
rkouakou06 <kouakou@insa-toulouse.fr>
2026-04-14 08:50:39 +0200

    add unit tests for Cluster properties and labeled/unlabeled enhancer

---
+++ isogroup/tests/test_unlabeled_enhancer.py
@@ -0,0 +1,58 @@
+import pytest
+import pandas as pd
+import numpy as np
+import isogroup.enhancer.unlabeled_enhancer as unlabeled_enhancer
+
+
+def test_annotate_feature_found():
+    """
+    Test the annotate_feature_found function of the unlabeled_enhancer module.
+
+    :param dataset_df: DataFrame containing the dataset for the experiment.
+    """
+    clusters_df = pd.DataFrame({'ClusterID': ['C0', 'C0', 'C1', 'C1', 'C1', 'C1', 'C1', 'C0', 'C0', 'C1', 'C1', 'C1', 'C1', 'C1'], 
+                                'FeatureID': ['F1', 'F2', 'F9', 'F8', 'F7', 'F6', 'F5', 'F1', 'F2', 'F9', 'F8', 'F7', 'F6', 'F5'], 
+                                'RT': [667.779067, 667.9255408, 676.8952154, 676.8898827, 676.6045604, 676.5620229, 676.4604364, 667.779067, 667.9255408, 676.8952154, 676.8898827, 676.6045604, 676.5620229, 676.4604364], 
+                                'm/z': [119.025753, 120.0291332, 133.0140851, 134.0174803, 135.0208168, 136.024129, 137.0275004, 119.025753, 120.0291332, 133.0140851, 134.0174803, 135.0208168, 136.024129, 137.0275004], 
+                                'sample': ['Sample_1', 'Sample_1', 'Sample_1', 'Sample_1', 'Sample_1', 'Sample_1', 'Sample_1', 'Sample_2', 'Sample_2', 'Sample_2', 'Sample_2', 'Sample_2', 'Sample_2', 'Sample_2'], 
+                                'Intensity': [1571414706.0, 0.0, 543216118.8, 2077278842.0, 3105587268.0, 2090662547.0, 529223407.9, 266171108.6, 129533534.2, 155940888.7, 218743897.0, 154077393.8, 97127965.25, 28994270.58], 
+                                'Isotopologue': ['Mx', 'Mx+1', 'Mx', 'Mx+1', 'Mx+2', 'Mx+3', 'Mx+4', 'Mx', 'Mx+1', 'Mx', 'Mx+1', 'Mx+2', 'Mx+3', 'Mx+4'], 
+                                'AlsoIn': ['[]', '[]', '[]', '[]', '[]', '[]', '[]', '[]', '[]', '[]', '[]', '[]', '[]', '[]']})
+    
+    result_df = unlabeled_enhancer.annotate_feature_found(clusters_df, sample_name="Sample_1")
+    assert "Found in unlabeled sample (Sample_1)" in result_df.columns
+    assert result_df["Found in unlabeled sample (Sample_1)"].tolist() == ["Yes", "No", "Yes", "Yes", "Yes", "Yes", "Yes", "Yes", "No", "Yes", "Yes", "Yes", "Yes", "Yes"]
+
+def test_sample_not_found():
+    """
+    Test the annotate_feature_found function of the unlabeled_enhancer module when the specified sample is not found.
+    """
+    clusters_df = pd.DataFrame({'ClusterID': ['C0', 'C0', 'C1', 'C1', 'C1', 'C1', 'C1'], 
+                                'FeatureID': ['F1', 'F2', 'F9', 'F8', 'F7', 'F6', 'F5'], 
+                                'RT': [667.779067, 667.9255408, 676.8952154, 676.8898827, 676.6045604, 676.5620229, 676.4604364], 
+                                'm/z': [119.025753, 120.0291332, 133.0140851, 134.0174803, 135.0208168, 136.024129, 137.0275004], 
+                                'sample': ['Sample_1', 'Sample_1', 'Sample_1', 'Sample_1', 'Sample_1', 'Sample_1', 'Sample_1'], 
+                                'Intensity': [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], 
+                                'Isotopologue': ['Mx', 'Mx+1', 'Mx', 'Mx+1', 'Mx+2', 'Mx+3', 'Mx+4'], 
+                                'AlsoIn': ['[]', '[]', '[]', '[]', '[]', '[]', '[]']})
+    
+    with pytest.raises(ValueError):
+        unlabeled_enhancer.annotate_feature_found(clusters_df, sample_name="Sample_2")
+
+def test_calculate_m1_m0_ratio():
+    """
+    Test the calculate_m1_m0_ratio function of the unlabeled_enhancer module.
+    """
+    clusters_df = pd.DataFrame({'ClusterID': ['C0', 'C0', 'C1', 'C1', 'C1', 'C1', 'C1'], 
+                                'FeatureID': ['F1', 'F2', 'F9', 'F8', 'F7', 'F6', 'F5'], 
+                                'RT': [667.779067, 667.9255408, 676.8952154, 676.8898827, 676.6045604, 676.5620229, 676.4604364], 
+                                'm/z': [119.025753, 120.0291332, 133.0140851, 134.0174803, 135.0208168, 136.024129, 137.0275004], 
+                                'sample': ['Sample_1', 'Sample_1', 'Sample_1', 'Sample_1', 'Sample_1', 'Sample_1', 'Sample_1'], 
+                                'Intensity': [1571414706.0, 0.0, 543216118.8, 2077278842.0, 3105587268.0, 2090662547.0, 529223407.9], 
+                                'Isotopologue': ['Mx', 'Mx+1', 'Mx', 'Mx+1', 'Mx+2', 'Mx+3', 'Mx+4'], 
+                                'AlsoIn': ['[]', '[]', '[]', '[]', '[]', '[]', '[]']})
+    
+    result_df = unlabeled_enhancer.calculate_m1_m0_ratio(clusters_df, sample_name="Sample_1")
+    assert "Mx+1/Mx ratio" in result_df.columns
+    assert (result_df.loc[result_df["ClusterID"] == "C0", "Mx+1/Mx ratio"] == "ND").all()
+    assert np.allclose(result_df.loc[result_df["ClusterID"] == "C1", "Mx+1/Mx ratio"], 3.8240, rtol=0, atol=1e-4)
